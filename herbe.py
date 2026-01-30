d419cfb9c137ad5c8d9edf2972571dec5e0e3bad
Eugene de Maistre <eugenedemaistre@gmail.com>
2026-01-30 10:20:39 +0100

    debut

---
+++ herbe.py
@@ -0,0 +1,13 @@
+import math
+
+def grille
+
+class Herbe():
+    def __init__(self,x,y):
+        self.x = x
+        self.y = y
+    
+    def mangee(self):
+        Grille.herbe(self.x,self.y) = 0
+
+    
