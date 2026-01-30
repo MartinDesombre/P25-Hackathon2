1f2786e1dcf6f150766b81a1727b05baedcb6fec
Eugene de Maistre <eugenedemaistre@gmail.com>
2026-01-30 10:49:26 +0100

    1e fin herbe

--- herbe.py
+++ herbe.py
@@ -1,13 +1,22 @@
-import math
+import random
+import numpy as np
 
-def grille
+
 
 class Herbe():
-    def __init__(self,x,y):
+    def __init__(self,x,y,tps_depuis_mort):
         self.x = x
         self.y = y
+        self.tps_depuis_mort = 0 if random.random() <= 0.3 else random.randint(1,7)
     
     def mangee(self):
         Grille.herbe(self.x,self.y) = 0
 
-    
+    def repousse(self):
+        if is_mouton(self.x,self.y):
+            pass
+        elif random.random() < 0.08 :
+            Grille.herbe(self.x,self.y) = 1
+        
+        elif tps_depuis_mort >= 7:
+            Grille.herbe(self.x,self.y) = 1
