3a9c3a1ac61a579e7b176d0c6e86eb8cbaed37d2
Martin Desombre <martin.desombre1@gmail.com>
2026-01-30 11:52:01 +0100

    update

--- src/p25_hackathon2/Grid.py
+++ src/p25_hackathon2/Grid.py
@@ -9,6 +9,7 @@ class Grid:
         self.cells = [random.choices(['.', '#'], weights=[GRASS_COVERAGE,1-GRASS_COVERAGE], k = size) for _ in range(size)]
 
     def update_grid(self, updates):
+        
         for list_mouton in Sheep.list():
             for sheep in list_mouton:
                 x, y = sheep[1], sheep[2]
@@ -20,6 +21,8 @@ class Grid:
                 x, y = wolf[1], wolf[2]
                 if wolf[0]=='alive':
                     self.cells[x][y] = 'W'
+
+    
 
             
 
